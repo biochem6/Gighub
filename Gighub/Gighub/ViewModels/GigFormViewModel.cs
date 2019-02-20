f0c852e588abafa7a46bb41f78111dfa78950ca2
biochem6 <aashland5@gmail.com>
2019-02-20 17:54:40 -0500

    Created Validation for date and time and enabled client-side jquery validation

--- Gighub/Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/Gighub/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,29 @@
 ﻿using Gighub.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
+
+
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
     }
 }
