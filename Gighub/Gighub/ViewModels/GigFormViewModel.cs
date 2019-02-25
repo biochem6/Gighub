082c97b9ece9a10b95400afe12936587b40eeb52
biochem6 <aashland5@gmail.com>
2019-02-25 18:42:27 -0500

    Implemented update and create pages.

--- Gighub/Gighub/ViewModels/GigFormViewModel.cs
+++ Gighub/Gighub/ViewModels/GigFormViewModel.cs
@@ -2,28 +2,45 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using Gighub.Controllers;
 
 namespace Gighub.ViewModels
 {
     public class GigFormViewModel
     {
-        [Required]
-        public string Venue { get; set; }
+        public int Id { get; set; }
 
-        [Required]
-        [FutureDate]
-        public string Date { get; set; }
+        [Required] public string Venue { get; set; }
 
-        [Required]
-        [ValidTime]
-        public string Time { get; set; }
+        [Required] [FutureDate] public string Date { get; set; }
 
-        [Required]
-        public byte Genre { get; set; }
+        [Required] [ValidTime] public string Time { get; set; }
+
+        [Required] public byte Genre { get; set; }
 
 
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> update = 
+                    (c => c.Update(this));
+
+                Expression<Func<GigsController, ActionResult>> create = 
+                    (c => c.Create(this));
+
+                var action = (Id != 0) ? update : create;
+                return (action.Body as MethodCallExpression)?.Method.Name;
+               
+            }
+        }   
+
+    public DateTime GetDateTime() => DateTime.Parse($"{Date} {Time}");
     }
 }
